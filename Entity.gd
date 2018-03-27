db232d3e8b0eff208ff9108c5d1da22795b890a8
Nick Jarvis <najarvis2016@gmail.com>
2018-03-27 13:01:12 -0400

    Added a few more tests. Decided this will essentially be my Godot Playground

--- Entity.gd
+++ Entity.gd
@@ -6,7 +6,7 @@ var vel = Vector2()
 export (int) var speed = 10
 export (String) var team = "Red"
 
-var max_speed = 400
+var max_speed = 300
 
 var carrying = null
 
@@ -16,23 +16,26 @@ func _ready():
 	pass
 
 func _process(delta):
-	var acc = Vector2()
+	var move_direction = Vector2()
 	if Input.is_key_pressed(KEY_W):
-		acc.y -= 1
+		move_direction.y -= 1
 	if Input.is_key_pressed(KEY_S):
-		acc.y += 1
+		move_direction.y += 1
 	if Input.is_key_pressed(KEY_A):
-		acc.x -= 1
+		move_direction.x -= 1
 	if Input.is_key_pressed(KEY_D):
-		acc.x += 1
+		move_direction.x += 1
 		
-	vel += acc.normalized()
-	if vel.length() > max_speed:
-		vel = vel.normalized() * max_speed
-	else:
-		vel *= 0.95
-	pos += vel * delta * speed
+	var acc_fac = 0.1
+	vel += ((move_direction.normalized() * max_speed) - vel) * acc_fac
+		
+	#vel += acc.normalized()
+	#if vel.length() > max_speed:
+	#	vel = vel.normalized() * max_speed
+	#else:
+	#	vel *= 0.95
 	
+	pos += vel * delta
 	position = pos
 	
 	for area in get_overlapping_areas():
---
+++ LightScript.gd
@@ -0,0 +1,11 @@
+extends Light2D
+
+var time = 0
+
+func _ready():
+
+	pass
+
+func _process(delta):
+	time += delta
+
